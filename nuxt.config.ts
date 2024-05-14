081879fc582bacbaef9a5b591eb418c9c97cbc26
Bruno Braga <brunobragaw8t@gmail.com>
2024-05-14 22:20:38 +0100

    chore: add ESLint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
