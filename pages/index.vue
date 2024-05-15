39953b72c876f56aa6f065a8fb1afbf2d90ad38f
Bruno Braga <brunobragaw8t@gmail.com>
2024-05-15 22:12:10 +0100

    feature: enable pages and create login+index routes

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+})
+</script>
+
+<template>
+  <div>
+    <h1>Index</h1>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+})
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <button>Click to login</button>
+  </div>
+</template>
