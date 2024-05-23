d9abd1b3518b8b0d71272c0cb1f9d938655a5766
Bruno Braga <brunobragaw8t@gmail.com>
2024-05-23 22:28:17 +0100

    wip: create call for MS Events

---
+++ components/MeetingsList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const auth = useAuth()
+const events = ref<any[]>([])
+
+onMounted(async () => {
+  console.log('go')
+
+  if (!auth.user.value) {
+    return
+  }
+  const { data, error } = await useFetch(
+    'https://graph.microsoft.com/v1.0/me/calendarview?startdatetime=2024-05-23T21:02:17.067Z&enddatetime=2024-05-30T21:02:17.067Z',
+    {
+      headers: {
+        Authorization: `Bearer ${auth.user.value.accessToken}`,
+      },
+    },
+  )
+
+  if (error) {
+    return
+  }
+
+  events.value = data.value as any[]
+})
+</script>
+
+<template>
+  <div>
+    <h1>Meetings List</h1>
+
+    {{ events }}
+
+    <!-- <ul> -->
+    <!--   <li -->
+    <!--     v-for="event in events" -->
+    <!--     :key="event.id" -->
+    <!--   > -->
+    <!--     <nuxt-link :to="`/meetings/${event.id}`"> -->
+    <!--       {{ event.title }} -->
+    <!--     </nuxt-link> -->
+    <!--   </li> -->
+    <!-- </ul> -->
+  </div>
+</template>
